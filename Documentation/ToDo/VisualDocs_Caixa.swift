171c56e6db1882cf640c7861128414a006b921ac
Ricardo Santos <rsantos@blissapplications.com>
2020-08-02 11:47:31 +0100

    Caixa

--- Documentation/ToDo/VisualDocs_Caixa.swift
+++ Documentation/ToDo/VisualDocs_Caixa.swift
@@ -1,7 +1,4 @@
 //
-//  Concept.Caixa.swift
-//  UglySwiftUIApp
-//
 //  Created by Ricardo Santos on 02/08/2020.
 //  Copyright © 2020 Ricardo P Santos. All rights reserved.
 //
@@ -49,7 +46,7 @@ extension V {
                 ZStack {
                     ZStack {
                         TextField(title, text: $value)
-                            .background(Color.white)
+                            //.background(Color.white)
                             .padding(.horizontal, size_1 / 2)
                             .padding(.vertical, size_1 / 3)
                             .border(Color.secondary, width: 2)
@@ -74,14 +71,20 @@ extension V {
 extension V {
     struct VisualDocs_Caixa_II: View {
         @State var rememberPassword = false
-
+        let topPartBackgroundView: Color
         var viewTop: some View {
-            ZStack { Color(colorPrimary)
+           ZStack {
+                topPartBackgroundView
                 VStack(alignment: .center, spacing: 20) {
                     HStack(spacing: 20) {
-                        Text("Internet".uppercased()).textColor(Color.white)
-                        Text("Banking".uppercased()).bold().textColor(Color.white)
-                    }
+                        Text("Internet".uppercased())
+                            .multilineTextAlignment(.trailing)
+                            .textColor(Color.white)
+                        Text("Banking".uppercased())
+                            .bold()
+                            .multilineTextAlignment(.leading)
+                            .textColor(Color.white)
+                    }.padding(.top, size_1)
                     Image("caixa_1").resizable().frame(width: size_1 * 2, height: size_1, alignment: .center)
                     Spacer()
                     HStack(spacing: 20) {
@@ -100,15 +103,14 @@ extension V {
                             }
                         }
                     }
-                    Spacer()
-                }.padding()
+                }.padding(.bottom, 30)
             }
         }
 
         var viewBottom: some View {
               VStack {
                   VStack {
-                      V.TextInputField(title: "User")
+                    V.TextInputField(title: "User")
                       Spacer()
                       V.TextInputField(title: "Password")
                       Button(action: {}) {
@@ -142,21 +144,39 @@ extension V {
                           //.background(Color(UIColor.gray))
                           .foregroundColor(Color(colorPrimary))
                       Text("Don't have an account".uppercased())
-                          .textColor(Color(colorPrimary))
-                          .frame(width: boxWidth, height: size_1*0.5)
-                          //.background(Color(UIColor.gray))
+                        .bold().font(.footnote)
+                        .textColor(Color(colorPrimary))
+                        .frame(width: boxWidth, height: size_1*0.5)
+                        //.background(Color(UIColor.gray))
                       Text("Accessibility mode".uppercased())
-                          .textColor(Color(colorPrimary))
-                          .frame(width: boxWidth, height: size_1*0.5)
-                          //.background(Color(UIColor.gray))
+                        .bold().font(.footnote)
+                        .textColor(Color(colorPrimary))
+                        .frame(width: boxWidth, height: size_1*0.5)
+                        //.background(Color(UIColor.gray))
                   }
               }
           }
-        
+
+        var viewMidle: some View {
+            ZStack {
+                topPartBackgroundView
+                Rectangle()
+                    .frame(width: UIScreen.screenWidth,
+                           height: size_1)
+                    .foregroundColor(Color(UIColor.white))
+                    .cornerRadius(size_1)
+                Color(UIColor.white).padding(.top, size_1 / 2)
+            }
+        }
+
+        let topPartHeight: CGFloat = 200
+        let clockBarSize: CGFloat = 44
+
         var body: some View {
-            ScrollView {
-                viewTop
-                viewBottom.cornerRadius(50)
+            VStack(alignment: .leading, spacing: 0) {
+                viewTop.padding(.top, -clockBarSize)
+                viewMidle
+                viewBottom
             }
         }
     }
@@ -166,6 +186,6 @@ extension V {
 
 struct VisualDocs_Caixa_II: PreviewProvider {
     static var previews: some View {
-        V.VisualDocs_Caixa_II()
+        V.VisualDocs_Caixa_II(topPartBackgroundView: Color(colorPrimary))
     }
 }
