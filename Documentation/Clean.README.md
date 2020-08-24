f9d98adacc5d0af709af45b3ee8b0b86a2bc927d
Ricardo Santos <rsantos@blissapplications.com>
2020-08-24 10:30:32 +0100

    deleted unrealated code

--- Documentation/Clean.README.md
+++
@@ -1,7 +0,0 @@
-
-
-Based on https://medium.com/swlh/clean-architecture-for-swiftui-6d6c4eb1cf6a
-
-#  Clean Architecture for SwiftUI
-
-
--- RJPS_SwiftUI/AppDelegate.swift
+++ RJPS_SwiftUI/AppDelegate.swift
@@ -7,19 +7,7 @@ import UIKit
 import SwiftUI
 import Combine
 //
-import App_Weather
-import App_HourlyChallenge
 import App_Ryanair
-//
-import Utils_Networking
-import Utils_Extensions
-
-enum TargetApp {
-    case sample
-    case weather
-    case hourly
-    case ryanair
-}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -28,27 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        //SampleAPI_Testing.test()
-        //CombineTesting.shared.test_AnyPublisher()
-        //CombineTesting.shared.testing_CurrentValueSubject()
-        //CombineTesting.shared.observe_UserDefaults()
-        //CombineTesting.shared.testing_handleEvents()
+        self.window?.rootViewController = UIHostingController(rootView: RyanairView1Builder.buildView())
+        //self.window?.rootViewController = UIHostingController(rootView: RyanairView1Builder.buildMockView())
 
-        //let testing = RyanairCoreTesting()
-        //testing.test1()
-
-        let targetApp: TargetApp = .ryanair
-        switch targetApp {
-        case .sample:
-            self.window?.rootViewController = UIHostingController(rootView: TabView_View())
-        case .hourly:
-            self.window?.rootViewController = UIHostingController(rootView: DashboardViewBuilder.buildView())
-        case .weather:
-            self.window?.rootViewController = UIHostingController(rootView: WeeklyWeatherBuilder.buildView())
-        case .ryanair:
-            self.window?.rootViewController = UIHostingController(rootView: RyanairView1Builder.buildView())
-            //self.window?.rootViewController = UIHostingController(rootView: RyanairView1Builder.buildMockView())
-        }
         return true
     }
 }
@@ -57,6 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 struct AppDelegate_Previews: PreviewProvider {
     static var previews: some View {
-        TabView_View()
+        RyanairView1Builder.buildView()
     }
 }
