3f1ec8301d8a9c509b2a2f1ffa8651f853504d03
Ricardo Santos <www.meumail@gmail.com>
2021-03-08 22:20:11 +0000

    code review

--- makeProject.sh
+++ makeProject.sh
@@ -4,3 +4,14 @@ open RJPS_SwiftUI.xcodeproj
 
 xcodegen dump --spec ./XcodeGen/RJPS_SwiftUI.yml --type graphviz --file ../Documents/Graph.viz
 xcodegen dump --spec ./XcodeGen/RJPS_SwiftUI.yml --type json --file ../Documents/Graph.json
+
+################################################################################
+
+echo 'periphery...'
+periphery scan
+
+################################################################################
+
+echo " ╔═══════════════════════╗"
+echo " ║ Done! You're all set! ║"
+echo " ╚═══════════════════════╝"
